faf00bff7c228357d4383d11803f161f77040a54
tirupathirao chinka <tirupathirao.chinka@gmail.com>
2020-08-07 00:55:24 +0100

    Latest changes

---
+++ server.js
@@ -0,0 +1,16 @@
+const express= require('express')
+const app= express();
+const path=require('path')
+
+const PORT=8000;
+
+
+// app.get('/',(req,res)=>{
+//     res.send('server');
+// })
+
+app.use(express.static(path.join(__dirname,'client','build')))
+
+app.listen(PORT,()=>{
+    console.log(`Server running ${PORT}`)
+})
